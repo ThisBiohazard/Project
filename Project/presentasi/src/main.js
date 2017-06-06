0e2d1e51a1c561c1cc7e311f8e42471cf6f713de
Irlan <edgex17@gmail.com>
2017-06-06 15:20:56 +0700

    penambahan materi

--- Project/presentasi/src/main.js
+++ Project/presentasi/src/main.js
@@ -28,6 +28,17 @@ bespoke.from('article', [
 require('prism')
 $('#p04').hide();
 $('#p05').hide();
+$('#q01').hide();
+$('#p02').hide();
+$('#q02').hide();
+$('#q03').hide();
+$('#q04').hide();
+$('#q05').hide();
 $(document).ready(function(){$(document).keyup(function(objEvent){(objEvent)?keycode=objEvent.keyCode:keycode=event.keyCode;if(keycode==81){$("#p02").slideToggle();}});});
 $(document).ready(function(){$(document).keyup(function(objEvent){(objEvent)?keycode=objEvent.keyCode:keycode=event.keyCode;if(keycode==87){$("#p04").slideToggle();}});});
 $(document).ready(function(){$(document).keyup(function(objEvent){(objEvent)?keycode=objEvent.keyCode:keycode=event.keyCode;if(keycode==69){$("#p05").slideToggle();}});});
+$(document).ready(function(){$(document).keyup(function(objEvent){(objEvent)?keycode=objEvent.keyCode:keycode=event.keyCode;if(keycode==82){$("#q01").slideToggle();}});});
+$(document).ready(function(){$(document).keyup(function(objEvent){(objEvent)?keycode=objEvent.keyCode:keycode=event.keyCode;if(keycode==84){$("#q02").slideToggle();}});});
+$(document).ready(function(){$(document).keyup(function(objEvent){(objEvent)?keycode=objEvent.keyCode:keycode=event.keyCode;if(keycode==89){$("#q03").slideToggle();}});});
+$(document).ready(function(){$(document).keyup(function(objEvent){(objEvent)?keycode=objEvent.keyCode:keycode=event.keyCode;if(keycode==85){$("#q04").slideToggle();}});});
+$(document).ready(function(){$(document).keyup(function(objEvent){(objEvent)?keycode=objEvent.keyCode:keycode=event.keyCode;if(keycode==73){$("#q05").slideToggle();}});});
